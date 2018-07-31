f951ee4275410dd3cb86c4aa7008e299957cff8c
pkboom <ppotpo@gmail.com>
2018-07-31 08:58:33 -0400

    remove comment below guarded

--- CreateGuarded.js
+++ CreateGuarded.js
@@ -1,27 +1,26 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 module.exports = class CreateGuarded {
     async create() {
-        
         if (this.activeEditor() === undefined) {
             return;
-        } 
-        
+        }
+
         let activeDocument = this.activeDocument().uri;
-        
+
         if (activeDocument === undefined) {
             return;
         }
-        
-        let fillable = await this.getFillable(activeDocument);
 
-        this.insertGuaded(fillable);
-    } 
-    
-    async getFillable(activeDocument) {
-        let fillable = {
+        let range = await this.getReplaceRange(activeDocument);
+
+        this.insertGuaded(range);
+    }
+
+    async getReplaceRange(activeDocument) {
+        let range = {
             start: 0,
-            end: 0,
+            end: 0
         };
 
         let classStartLine = 0;
@@ -31,48 +30,58 @@ module.exports = class CreateGuarded {
         for (let line = 0; line < doc.lineCount; line++) {
             let textLine = doc.lineAt(line).text.trim();
 
-            if (!fillable.start && /fillable/.test(textLine)) {
-                fillable.start = line;
-            }
-            
-            if (!!fillable.start && /];/.test(textLine)) {
-                fillable.end = line + 1;
-                
-                break;
-            }
-            
             if (/class/.test(textLine)) {
-                classStartLine = textLine.endsWith('{') ? line + 1 : line + 2;
-            }
-            
-            if (/use/.test(textLine)) {
-                classStartLine = line + 2;
-            }
-
-            if (/}/.test(textLine)) {
-                fillable.start = classStartLine;
-                fillable.end = classStartLine;
+                classStartLine = textLine.endsWith("{") ? line + 1 : line + 2;
 
                 break;
             }
         }
 
-        return fillable;
+        // Starting after 'class {'
+        for (let line = classStartLine; line < doc.lineCount; line++) {
+            let textLine = doc.lineAt(line).text.trim();
+
+            if (/\/\//.test(textLine)) {
+                range.start = classStartLine;
+                range.end = classStartLine + 1;
+
+                break;
+            }
+
+            if (!range.start && /fillable/.test(textLine)) {
+                range.start = line;
+            }
+
+            if (!!range.start && /];/.test(textLine)) {
+                range.end = line + 1;
+
+                break;
+            }
+        }
+
+        if (!range.start) {
+            range.start = classStartLine;
+            range.end = classStartLine;
+        } 
+
+        return range;
     }
-    
-    insertGuaded(fillable) {
-        console.log(fillable);
-        let snippet = '\tprotected \\$guarded = [];\$1\n';
+
+    insertGuaded(range) {
+        console.log(range);
+        let snippet = "\tprotected \\$guarded = [];$1\n";
 
         this.activeEditor().insertSnippet(
             new vscode.SnippetString(snippet),
-            this.range(fillable.start, fillable.end)
+            this.range(range.start, range.end)
         );
-
     }
 
     range(startLine, endLine) {
-        return new vscode.Range(new vscode.Position(startLine, 0), new vscode.Position(endLine, 0))
+        return new vscode.Range(
+            new vscode.Position(startLine, 0),
+            new vscode.Position(endLine, 0)
+        );
     }
 
     activeEditor() {
@@ -84,6 +93,6 @@ module.exports = class CreateGuarded {
     }
 
     config(key) {
-        return vscode.workspace.getConfiguration('phpTestCreator').get(key);
+        return vscode.workspace.getConfiguration("phpTestCreator").get(key);
     }
-}
+};
